dc5d5b411771b613917333beaa8aecdb76779e68
Josh Burrill <joshua.burrill.1@gmail.com>
2020-11-08 09:05:37 -0500

    Fixed styling for MapEditorContainer

--- src/map/MapEditorContainer.js
+++ src/map/MapEditorContainer.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import MapEditor from "./MapEditor";
+import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  mapNameStyle: {}
+});
 
 export default function MapEditorContainer(){
+  const classes = useStyles()
 
   return (
     <Container style={{ textAlign: "center" }}>
+      <Typography className={classes.mapNameStyle}>
+              <a href="https://fontmeme.com/pokemon-font/">
+                <img
+                  src="https://fontmeme.com/permalink/201105/1750b3325da4237a1c5ef5d60e9a67df.png"
+                  alt="pokemon-font"
+                  border="0"
+                  style={{ height: "50px", width: "200px" }}
+                />
+              </a>
+      </Typography>
       <MapEditor />
     </Container>
   );
